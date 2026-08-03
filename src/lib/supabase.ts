5c9b13800884c5d44c3f31b6f6f6f1de3ea8953e
balach32 <164952248+bakwas76@users.noreply.github.com>
2026-08-03 09:19:37 -0700

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,29 +1,14 @@
-// import { createClient } from '@supabase/supabase-js';
-
-// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-// const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
-
-// if (!supabaseUrl || !supabaseAnonKey) {
-//   throw new Error('Missing Supabase environment variables. Check your .env file.');
-// }
-
-// export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-//   auth: {
-//     persistSession: true,
-//     autoRefreshToken: true,
-//     detectSessionInUrl: true,
-//   },
-// });
-
-
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl =
-  import.meta.env.VITE_SUPABASE_URL ||
-  "https://dwcckjvpzobpabielbsz.supabase.co";
+const supabaseUrl = 'https://qzndrmlvckbsziydgsnh.supabase.co';
 
 const supabaseAnonKey =
-  import.meta.env.VITE_SUPABASE_ANON_KEY ||
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3Y2NranZwem9icGFiaWVsYnN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxMjk2MjEsImV4cCI6MjEwMDcwNTYyMX0.ikenSo4oHY9_V9h7WHlpnPLnJ2-kZD6cNARaazN5DHI";
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6bmRybWx2Y2tic3ppeWRnc25oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU3NjI4NzcsImV4cCI6MjEwMTMzODg3N30.ggx0VDvby4EA51VY3esv29E6ORXBh6q6kXBWxfBZXcc';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+  },
+});
